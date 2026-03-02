bfed136e91cc3a8b71eec1c6ccab0d35d1de0ee7
jimcoder-005 <jimmyysin@gmail.com>
2026-03-02 20:24:02 +0800

    Update AttendanceProject.py

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -69,13 +69,4 @@ while True:
     cv2.waitKey(1)
 
 
-# faceloc=face_recognition.face_locations(imgElon)[0]
-# encodeElon=face_recognition.face_encodings(imgElon)[0]
-# cv2.rectangle(imgElon,(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(255,0,255),3)
 
-# facelocTest=face_recognition.face_locations(imgTest)[0]
-# encodeTest=face_recognition.face_encodings(imgTest)[0]
-# cv2.rectangle(imgTest,(facelocTest[3],facelocTest[0]),(facelocTest[1],facelocTest[2]),(255,0,255),2)
-
-# results= face_recognition.compare_faces([encodeElon],encodeTest)
-# faceDis= face_recognition.face_distance([encodeElon],encodeTest)
